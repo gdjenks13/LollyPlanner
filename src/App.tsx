c87c0c3f985be6500b95b22df1c834c94c0ab1cb
gdjenks13 <gdjenks13@gmail.com>
2025-01-22 09:59:45 -0500

    initial commit. setup react + vite + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-xl">LollyPlanner</h1>
+      <p className="text-base">A suite of financial calculation and planning tools</p>
+    </>
+  )
+}
+
+export default App
